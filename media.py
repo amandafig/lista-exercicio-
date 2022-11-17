f51448486fa8993a2cd539d2e9d87ab8367791ba
amandafig <111015122+amandafig@users.noreply.github.com>
2022-11-17 14:57:46 -0300

    média

---
+++ media.py
@@ -0,0 +1,9 @@
+from msilib.schema import Media
+
+
+NOTABI1=float(input("Digite a nota do 1º bimsntre: "))
+NOTABI2=float(input("Digite a nota do 2º bimsntre: "))
+NOTABI3=float(input("Digite a nota do 3º bimsntre: "))
+NOTABI4=float(input("Digite a nota do 4º bimsntre: "))
+Media=(NOTABI1+NOTABI2+NOTABI3+NOTABI4)/4
+print("A média total é: ",Media) 
